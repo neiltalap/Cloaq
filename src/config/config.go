8642a0ce9711e04028a77f9f1f97f0f7683f6109
neiltalap <khair_talap@outlook.com>
2026-04-01 00:18:12 +0500

    chore(license): switch to agpl and simplify headers

--- src/config/config.go
+++ src/config/config.go
@@ -1,16 +1,4 @@
-// NOTICE
-
-// Project Name: Cloaq
-// Copyright © 2026 Neil Talap and/or its designated Affiliates.
-
-// This software is licensed under the Dragonfly Public License (DPL) 1.0.
-
-// All rights reserved. The names "Neil Talap" and any associated logos or branding
-// are trademarks of the Licensor and may not be used without express written permission,
-// except as provided in Section 7 of the License.
-
-// For commercial licensing inquiries or permissions beyond the scope of this
-// license, please create an issue in github.
+// © 2026 Polypodomy — AGPLv3
 
 package config
 
