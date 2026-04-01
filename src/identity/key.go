88da2307a9d35f805df21820a0a33c5a6f2a6e36
neiltalap <khair_talap@outlook.com>
2026-04-02 00:09:34 +0500

    feat(key): save and load functionality of the key

--- src/identity/key.go
+++ src/identity/key.go
@@ -2,7 +2,11 @@ package key
 
 import (
 	"crypto/rand"
+	"errors"
+	"os"
 )
+
+var WrongByteSize = errors.New("size of the key is not 32 bytes")
 
 type PrivateKey struct {
 	Data [32]byte
@@ -15,4 +19,29 @@ func (privateKey *PrivateKey) Generate() error {
 	}
 
 	return nil
+}
+
+func (privateKey *PrivateKey) Save(filePath string) error {
+	if err := os.WriteFile(filePath, privateKey.Data[:], 0600); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Load(filePath string) (*PrivateKey, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) != 32 {
+		return nil, WrongByteSize
+	}
+
+	tempPrivateKey := new(PrivateKey)
+	// copying the data read from the file to the instance
+	copy(tempPrivateKey.Data[:], data)
+
+	return tempPrivateKey, nil
 }
